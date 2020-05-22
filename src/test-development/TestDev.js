035e84ef594b00f22fd04f0073fb14bac37d0bc1
ajc24 <anthonyjohnc@gmail.com>
2020-05-22 18:27:28 +0100

    add initial unit test helper modules

---
+++ src/test-development/TestDev.js
@@ -0,0 +1,64 @@
+import { mount, shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+export default class TestDev {
+  /**
+   * Generates a JSON snapshot for the specified React component
+   * @param {React.Component} component
+   * @returns {JSON}
+   */
+  static createSnapshot(component) {
+    return renderer.create(component).toJSON();
+  }
+  
+  /**
+   * Mounts the specified React component using enzyme
+   * @param {React.Component} component
+   * @returns {ReactWrapper}
+   */
+  static mount(component) {
+    return mount(component);
+  }
+
+  /**
+   * Mounts the specified React component using enzyme
+   * and produces the HTML result from that component
+   * @param {React.Component} component
+   * @returns {string}
+   */
+  static mountHtml(component) {
+    const wrapper = this.mount(component);
+    return wrapper.html();
+  }
+
+  /**
+   * Mounts the specified React component inside a HTML template
+   * and produces the entire HTML template including the component
+   * @param {React.component} component
+   * @param {string} htmlTitle
+   * @returns {string}
+   */
+  static mountHtmlTemplate(component, htmlTitle = 'Mounted HTML Template') {
+    return `<!DOCTYPE html><html lang="en"><head><title>${htmlTitle}</title></head><body>${this.mountHtml(component)}</body></html>`;
+  }
+
+  /**
+   * Shallow renders the specified React component using enzyme
+   * @param {React.Component} component
+   * @returns {ReactWrapper}
+   */
+  static shallow(component) {
+    return shallow(component);
+  }
+
+  /**
+   * Shallow renders the specified React component using enzyme
+   * and produces the HTML result from that component
+   * @param {React.Component} component
+   * @returns {string}
+   */
+  static shallowHtml(component) {
+    const wrapper = this.shallow(component);
+    return wrapper.html();
+  }
+}
